4b27b020ece6577fa00ea4417ae199115eb83e2c
Jaime Mansilla Chao <jaime.mansilla@lasallesantiago.gal>
2020-08-08 00:59:49 +0200

    Revert "commit email-redactar"
    This reverts commit caca86e80f50d737128cb14a672ad0efaa1fb896.

--- routes/api.php
+++ routes/api.php
@@ -14,11 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-/* Route::middleware('auth:api')->get('/user', function (Request $request) {
+Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
-}); */
-
-Route::group(['prefix' => 'mail'], function () {
-    Route::get('/', 'EmailController@index');
-    Route::post('/', 'EmailController@store');
 });
